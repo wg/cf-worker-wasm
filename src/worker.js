6fdce356a7c60be122ba3604dcd46531786f125f
Will <will@glozer.net>
2022-08-13 11:00:59 +0900

    upgrade wrangler to 2.0.25

--- src/worker.js
+++ src/worker.js
@@ -1,10 +1,12 @@
 import module from './wasm/greet_bg.wasm';
 import init, { greet } from './wasm/greet.js';
 
-const instance = await init(module);
+const instance = init(module);
 
 export default {
     async fetch(request) {
+        await instance;
+
         let name  = request.url.split('/').pop();
         let hello = greet(name);
         return new Response(hello);
